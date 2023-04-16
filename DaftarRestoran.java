82664b431ab3efb47efd2a417e669cfc40940c10
Anastasiamunthe <114917804+Anastasiamunthe@users.noreply.github.com>
2023-04-16 16:33:48 +0800

    Create DaftarRestoran.java

---
+++ DaftarRestoran.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DaftarRestoran {
+
+    public static void main(String[] args) {
+        System.out.println("1. Restoran Padang");
+        System.out.println("2. Restoran Casino");
+        System.out.println("3. Hoodali Resto");
+        System.out.println("4. Restoran Minang");
+        System.out.println("5. Restoran Barat");
+        System.out.println("6. Family Resto");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Pilihan Anda : ");
+        int pilihan = input.nextInt();
+
+
+    }
+}
